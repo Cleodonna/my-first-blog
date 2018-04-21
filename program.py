4c67f8fece69c4779a90adec885cd127993f11ed
Viky <cleodonna@gmail.com>
2018-04-21 15:40:27 +0300

    My Django Girls app, first commit

---
+++ program.py
@@ -0,0 +1,19 @@
+
+
+#print(lottery)
+#lottery.pop(1)
+#print(participant['name'])
+
+def printName():
+    name = 'Sandra'
+    lottery = [3, 42, 12, 19, 30, 59
+    participant = {'name': 'Ola', 'country': 'Poland', 'favorite_numbers': [7, 42, 92]}
+
+    if name == 'sandra':
+        print('conditia 1')
+    elif name == 'Sandra':
+        print('conditia 2')
+    else:
+        print('nicio conditie')
+
+#printName()
